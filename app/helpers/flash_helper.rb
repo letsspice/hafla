a9afbf70233fc756af28b3a8c558162e4979bdb3
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2025-05-17 15:13:35 +0300

    fix rubocop offences

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,58 +1,40 @@
 module FlashHelper
+  NOTIFICATION_CLASSES = {
+    'alert' => 'border-red-500 bg-red-50 text-red-500',
+    'notice' => 'border-blue-500 bg-blue-50 text-blue-500',
+    'success' => 'border-green-500 bg-green-50 text-green-500',
+    'error' => 'border-red-500 bg-red-50 text-red-500',
+    'warning' => 'border-yellow-500 bg-yellow-50 text-yellow-500',
+    'info' => 'border-blue-500 bg-blue-50 text-blue-500'
+  }.freeze
+
+  NOTIFICATION_ICONS = {
+    'alert' => 'fa-solid fa-circle-exclamation',
+    'notice' => 'fa-solid fa-info-circle',
+    'success' => 'fa-solid fa-check-circle',
+    'error' => 'fa-solid fa-exclamation-triangle',
+    'warning' => 'fa-solid fa-circle-exclamation',
+    'info' => 'fa-solid fa-circle-info'
+  }.freeze
+
+  PROGRESS_BAR_BACKGROUNDS = {
+    'alert' => 'bg-red-500',
+    'notice' => 'bg-blue-500',
+    'success' => 'bg-green-500',
+    'error' => 'bg-red-500',
+    'warning' => 'bg-yellow-500',
+    'info' => 'bg-blue-500'
+  }.freeze
+
   def notification_class(type)
-    case type
-    when 'alert'
-      'border-red-500 bg-red-50 text-red-500'
-    when 'notice'
-      'border-blue-500 bg-blue-50 text-blue-500'
-    when 'success'
-      'border-green-500 bg-green-50 text-green-500'
-    when 'error'
-      'border-red-500 bg-red-50 text-red-500'
-    when 'warning'
-      'border-yellow-500 bg-yellow-50 text-yellow-500'
-    when 'info'
-      'border-blue-500 bg-blue-50 text-blue-500'
-    else
-      'border-gray-500 bg-gray-50 text-gray-500'
-    end
+    NOTIFICATION_CLASSES[type] || 'border-gray-500 bg-gray-50 text-gray-500'
   end
 
   def notification_icon(type)
-    case type
-    when 'alert'
-      'fa-solid fa-circle-exclamation'
-    when 'notice'
-      'fa-solid fa-info-circle'
-    when 'success'
-      'fa-solid fa-check-circle'
-    when 'error'
-      'fa-solid fa-exclamation-triangle'
-    when 'warning'
-      'fa-solid fa-circle-exclamation'
-    when 'info'
-      'fa-solid fa-circle-info'
-    else
-      'fa-solid fa-circle-info'
-    end
+    NOTIFICATION_ICONS[type] || 'fa-solid fa-circle-info'
   end
 
   def progress_bar_background(type)
-    case type
-    when 'alert'
-      'bg-red-500'
-    when 'notice'
-      'bg-blue-500'
-    when 'success'
-      'bg-green-500'
-    when 'error'
-      'bg-red-500'
-    when 'warning'
-      'bg-yellow-500'
-    when 'info'
-      'bg-blue-500'
-    else
-      'bg-gray-500'
-    end
+    PROGRESS_BAR_BACKGROUNDS[type] || 'bg-gray-500'
   end
 end
