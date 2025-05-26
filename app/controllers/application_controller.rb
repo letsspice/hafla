1ad88d0e4fbe71145e1b51e2f582bcab6dbf9c8d
Annastacia <annetotoh@gmail.com>
2025-05-26 14:41:07 +0300

    configure subdomain redirection to organizations show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, unless: :devise_controller?
-  before_action :ensure_organization_profile_complete, if: :user_signed_in?
+  before_action :load_organization_by_subdomain
+  before_action :authenticate_user!, if: :admin_request?
+  before_action :ensure_organization_profile_complete, if: -> { admin_request? && user_signed_in? }
 
   private
 
@@ -8,11 +9,25 @@ class ApplicationController < ActionController::Base
     new_organization_path
   end
 
+  def admin_request?
+    byebug
+    request.subdomain.blank? || request.subdomain == 'www'
+  end
+
   def ensure_organization_profile_complete
     return if devise_controller? || request.path == new_organization_path || request.path == destroy_user_session_path
-
     return unless current_user.organizations.empty?
 
     redirect_to new_organization_path, alert: 'Please complete your organization profile to continue.'
+  end
+
+  def load_organization_by_subdomain
+    return if request.subdomain.blank? || request.subdomain == 'www'
+
+    @organization = Organization.find_by(subdomain: request.subdomain)
+
+    unless @organization
+      render plain: "Organization not found", status: :not_found
+    end
   end
 end
