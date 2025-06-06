395887f0830b9e1d02a413b7e0260eefd6d20513
Annastacia <annetotoh@gmail.com>
2025-06-06 11:26:11 +0300

    helper for sidebar links for the organization

--- Gemfile
+++ Gemfile
@@ -23,8 +23,8 @@ gem 'rack-session', '>= 2.1.1'
 gem 'rails', '~> 7.1.5', '>= 7.1.5.1'
 gem 'rubocop', '1.75.3'
 gem 'sprockets-rails', '3.5.2'
-gem 'stringio', '~> 3.1.7'
 gem 'stimulus-rails', '1.3.4'
+gem 'stringio', '~> 3.1.7'
 gem 'tailwindcss-rails', '~> 2.6'
 gem 'turbo-rails', '2.0.13'
 gem 'tzinfo-data', platforms: %i[windows jruby]
@@ -53,4 +53,4 @@ group :test do
   gem 'capybara', '3.40.0'
   gem 'selenium-webdriver', '4.31.0'
 end
-gem "redis"
+gem 'redis'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,12 @@
 # app/controllers/home_controller.rb
 class HomeController < ApplicationController
-    before_action :authenticate_user!
-  
-	def index
-		if current_user.org_admin?
-			redirect_to organization_admin_organization_path(current_user.organizations.first)
-		else
-			render :index
-		end
-	end
+  before_action :authenticate_user!
+
+  def index
+    if current_user.org_admin?
+      redirect_to organization_admin_organization_path(current_user.organizations.first)
+    else
+      render :index
+    end
+  end
 end
-  
