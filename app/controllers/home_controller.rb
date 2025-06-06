15eb5493f9872071fb4eeb7a32208987da489cd8
Annastacia <annetotoh@gmail.com>
2025-06-06 09:22:00 +0300

    navigation layouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
+# app/controllers/home_controller.rb
 class HomeController < ApplicationController
+    before_action :authenticate_user!
+  
+	def index
+		if current_user.org_admin?
+			redirect_to organization_admin_organization_path(current_user.organizations.first)
+		else
+			render :index
+		end
+	end
 end
+  
